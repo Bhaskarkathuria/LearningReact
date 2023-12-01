6979a36ee518c5ed13d21a98e6b26df8f54d1a47
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-12-01 18:10:19 +0530

    LearningReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import Card from './components/Card'
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "car insuranse",
+      amount: 1000,
+      date: new Date(2023, 7, 15),
+    },
+    {
+      id: 2,
+      title: "movie",
+      amount: 50,
+      date: new Date(2023, 7, 25),
+    },
+    {
+      id: 3,
+      title: "medical",
+      amount: 500,
+      date: new Date(2023, 5, 5),
+    },
+    {
+      id: 4,
+      title: "house rent",
+      amount: 300,
+      date: new Date(2023, 7, 30),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="App">
+      <h1>Lets Get Started!</h1>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </Card>
   );
 }
 
